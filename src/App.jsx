0256ceb806f10012388bcb02a6649be035d7fc1b
Arjun Gulati <agulati2028@gmail.com>
2025-12-08 13:38:32 -0500

    Under construction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import igemLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
@@ -9,24 +8,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={igemLogo} className="logo" alt="iGEM logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Lambert iGEM</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This website is under construction.
       </p>
     </>
   )
